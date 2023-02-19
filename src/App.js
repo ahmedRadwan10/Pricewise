f7134c934bf72b717f9f125e1c141847df1792d6
Ahmed Radwan <ahmedrhd36@gmail.com>
2023-02-19 19:27:37 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Routes/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+            <Header />
+            <Routes>
+                  <Route path="/" element={<Home />} />
+            </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.main_container}>
+            <div className={styles.logo}>
+                <p>Pricewise</p>
+            </div>
+            <div className={styles.categories}>
+                <i className="fa-solid fa-bars"></i>
+                Categories
+            </div>
+            <div className="search">
+                <input type="text" placeholder='What are you looking for?' autoComplete='off' />
+            </div>
+            <div>
+                <nav>
+                    <span className={styles.ar}>العربية</span>
+                    <span className='column_divider'></span>
+                    <span className={styles.bell}><i className="fa-regular fa-bell"></i></span>
+                    <span className='column_divider'></span>
+                    <span className={styles.star}><i className="fa-regular fa-star"></i></span>
+                    <span className='column_divider'></span>
+                    <span><i className="fa-regular fa-user"></i></span>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
