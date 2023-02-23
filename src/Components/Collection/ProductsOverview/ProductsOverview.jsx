d4f5698832ebb35dc018d4d8c8e462c62e2b2454
Ahmed Radwan <ahmedrhd36@gmail.com>
2023-02-23 04:05:24 +0300

    Update ProductsOverview

--- src/Components/Collection/ProductsOverview/ProductsOverview.jsx
+++ src/Components/Collection/ProductsOverview/ProductsOverview.jsx
@@ -20,11 +20,11 @@ const ProductsOverview = ({ data }) => {
     const renderProductFooter = (product) => {
         return (
             <>
+                 <button className={styles.fav_btn}><img src="/assets/imgs/icons/heart.svg" alt="Fav-Icon" /></button>
                 { product.reviews_number ? <div className={styles.rating_container}>
                     <i className="fa-solid fa-star fa-sm"></i>
                     <span>{product.rating}</span>
                 </div> : <div className={styles.rating_container}> <i className="fa-solid fa-star fa-sm"></i> 0.0</div>}
-                { product.old_price > product.new_price ? <div className={styles.price_change}>{Math.floor(100 - (product.new_price / product.old_price) * 100)}<span>%</span> <i className="fa-solid fa-arrow-trend-down"></i></div> : <div className={styles.price_change_negative}>{Math.floor(100 - (product.old_price / product.new_price) * 100)}<span>%</span> <i className="fa-solid fa-arrow-trend-up"></i></div>}
             </>
         );
     }
@@ -36,14 +36,17 @@ const ProductsOverview = ({ data }) => {
                         key={product["web-scraper-order"]}
                         className={styles.product_container}
                         onClick={() => handleProductOnClick(product)}
-                        >
+                >
                         <div className={styles.product_img_container}>
                             <Image imgSrc={`/assets/imgs/products/product.png`} imgAlt={product.title} />
                         </div>
                         <p title={product.title}>{product.title}</p>
-                        <div className={styles.new_price_container}>
-                            <span>{product.new_price.toFixed(2)}</span>
-                            <span>EGP</span>
+                        <div className={styles.price_container}>
+                            <div className={styles.new_price_container}>
+                                <span>{product.new_price.toFixed(2)}</span>
+                                <span>EGP</span>
+                            </div>
+                            { product.old_price > product.new_price ? <div className={styles.price_change}>{Math.floor(100 - (product.new_price / product.old_price) * 100)}<span>%</span> <i className="fa-solid fa-arrow-trend-down"></i></div> : <div className={styles.price_change_negative}>{Math.floor(100 - (product.old_price / product.new_price) * 100)}<span>%</span> <i className="fa-solid fa-arrow-trend-up"></i></div>}
                         </div>
                         {product.old_price ? renderProductOldPrice(product) : ""}
                         <div className={styles.product_footer}>
@@ -59,11 +62,11 @@ const ProductsOverview = ({ data }) => {
     }
 
     const scrollProductsToLeft = () => {
-        productsContainer.current.scrollLeft -= 300;
+        productsContainer.current.scrollLeft -= 500;
     }
 
     const scrollProductsToRight = () => {
-        productsContainer.current.scrollLeft += 300;
+        productsContainer.current.scrollLeft += 500;
     }
 
     if (data) return (
