c2f8c0509f45dbb2d40451b18e5a178eff4f53a3
Samir <mohamedsamir979797@gmail.com>
2023-05-04 13:03:02 +0300

    profile update

--- src/Components/Header/SubComponents/Wiishlist_Profile/subcomponents/Profile.jsx
+++ src/Components/Header/SubComponents/Wiishlist_Profile/subcomponents/Profile.jsx
@@ -1,29 +1,71 @@
 import { useSelector, useDispatch } from "react-redux";
 import { logOut } from "../../../../../redux/slices/authSlice";
 import styles from "../Wishlist_Profile.module.css";
+import { useState, useEffect } from "react";
+
 const Profile = () => {
   const user = useSelector(({ authState }) => authState.user);
+  const [profileButton, setProfileButton] = useState("Update");
+  const [formInput, setFormInput] = useState({
+    first_name: user.firstName,
+    last_name: user.lastName,
+    email: user.email,
+  });
+
+  const handleUserInput = (name, value) => {
+    setFormInput({
+      ...formInput,
+      [name]: value,
+    });
+  };
 
   return (
     <>
       <div className={styles.profile}>
-        <header>
-          <ul>
-            <li>Overview</li>
-          </ul>
-        </header>
-        <section>
-          <div>
-            <h3>Name</h3>
-            <p>{user.firstName + " " + user.lastName}</p>
+        <form className={styles.modal__form}>
+          <div className={styles.name_field}>
+            <div>
+              <label htmlFor="fname">First Name</label>
+              <input
+                type="text"
+                id="fname"
+                name="first_name"
+                value={formInput.first_name}
+                onChange={({ target }) => {
+                  handleUserInput(target.name, target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="lname">Last Name</label>
+              <input
+                type="text"
+                id="lname"
+                name="last_name"
+                value={formInput.last_name}
+                onChange={({ target }) => {
+                  handleUserInput(target.name, target.value);
+                }}
+              />
+            </div>
           </div>
-          <div>
-            <h3>Email</h3>
-            <p>{user.email}</p>
+          <div className={styles.form_field}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formInput.email}
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
+            />
           </div>
-        </section>
+          <button className={styles.profile_button}>{profileButton}</button>
+        </form>
       </div>
     </>
   );
 };
+
 export default Profile;
